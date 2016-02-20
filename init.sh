b96dc314b558425c8ea92cb3faf6382505e97ca3
Amey Jadiye <ameyjadiye@gmail.com>
2016-02-20 20:10:04 +0530

    adding few packages when installed fresh copy of linux mint 17.3

---
+++ init.sh
@@ -0,0 +1,15 @@
+     sudo  apt-get update
+     sudo  apt-get  install htop
+     sudo  apt-get  install screenfetch
+     sudo add-apt-repository ppa:djcj/screenfetch
+     sudo  apt-get update
+     sudo  apt-get  install screenfetch
+     sudo apt-add-repository ppa:numix/ppa
+     sudo apt-get update
+     sudo apt-get install numix-icon-theme-circle
+     sudo apt install terminator
+     sudo apt-get install vim
+     sudo apt-get install iptraf
+     sudo apt-get update
+     sudo apt-get install  vokoscreen
+     sudo apt-get install git
