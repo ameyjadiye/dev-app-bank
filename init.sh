1185fcc95f082164915cd01f39089aeaa45defd5
Amey Jadiye <ameyjadiye@gmail.com>
2016-02-21 21:49:38 +0530

    adding stickynotes

--- init.sh
+++ init.sh
@@ -19,4 +19,6 @@
      ruby -version
      gem install jekyll
      sudo apt-get install nodejs nodejs-dev npm
+     sudo apt-add-repository ppa:umang/indicator-stickynotes
+     sudo apt-get install indicator-stickynotes
 
