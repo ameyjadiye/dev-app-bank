34a060b1f948a3fed987c89a5682679b754ee4a8
Amey Jadiye <ameyjadiye@gmail.com>
2016-02-21 18:00:32 +0530

    installing ruby along with few imp gems (rails,jekyll,puma) & nodejs,npm

--- init.sh
+++ init.sh
@@ -13,3 +13,10 @@
      sudo apt-get update
      sudo apt-get install  vokoscreen
      sudo apt-get install git
+     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+     \curl -sSL https://get.rvm.io | bash -s stable --ruby --gems=rails,puma
+     source /home/amey/.rvm/scripts/rvm
+     ruby -version
+     gem install jekyll
+     sudo apt-get install nodejs nodejs-dev npm
+
